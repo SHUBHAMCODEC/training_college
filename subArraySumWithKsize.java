ee0e79cb6b9a65aee39b8b40026b38972a17ee0a
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-12 15:28:23 +0000

    comment

--- subArraySumWithKsize.java
+++ subArraySumWithKsize.java
@@ -24,7 +24,7 @@ public class subArraySumWithKsize {
         return maxSum;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// main function
         int arr[] = {1, 5, 6, 8, 2, 3, 4, 7, 10, 15, 11, 17, 13};
         int k = 3;
         System.out.println("sum is " + helper(arr, k)); 
