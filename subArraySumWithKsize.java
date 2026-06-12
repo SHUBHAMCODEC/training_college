84a2285a391c6281ec22c8ace6ba6ad47e5521ba
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-12 15:25:33 +0000

    add function

--- subArraySumWithKsize.java
+++ subArraySumWithKsize.java
@@ -1,7 +1,20 @@
 public class subArraySumWithKsize {
 
     private static void helper(int [] arr,int k){
-        
+        int sum=0;
+
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+
+        int maxSum=0;
+        for(int i=k;i<arr.length;i++){
+            sum+=arr[i];
+            maxSum=sum-arr[i-k];
+            maxSum=Math.max(sum,maxSum);
+        }
+
+        return maxSum;
     }
     public static void main(String[] args) {
         
