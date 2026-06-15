d192c12eb3af8992d23024de69b5f51668d7682d
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-15 16:02:02 +0000

    more question

--- thoughtWorks/getMinDiff.java
+++ thoughtWorks/getMinDiff.java
@@ -15,11 +15,11 @@ public class getMinDiff {
             temp1[i]=arr[l+i];
         }
         for(int i=0;i<n2;i++){
-            temp1[i]=arr[m+1+i];
+            temp2[i]=arr[m+1+i];
         }
 
 
-        int i=0,j=0,k=0;
+        int i=0,j=0,k=l;
 
         while(i!=temp1.length && j!=temp2.length){
             if(temp1[i]<temp2[j]){
@@ -44,20 +44,46 @@ public class getMinDiff {
 
     private static void mergeSort(int[] arr,int l,int h){
 
-        while(l<h){
+        if(l<h){
             int mid=l+((h-l)/2);
 
 
             mergeSort(arr, l, mid);
             mergeSort(arr, mid+1, h);
 
-            merge(arr,l,m,h);
+            merge(arr,l,mid,h);
+
+            
         }
     }
-    private static void helper(int arr[]){
-        mergeSort(arr,0,arr.length);
+    private static void helper(int arr[],int k){
+         int n = arr.length;
+        if (n == 1) {
+            System.out.println("Minimum heights: 0");
+            return;
+        }
+        mergeSort(arr,0,n-1);
+
+        int ans=arr[n-1]-arr[0];
+
+        int lowest=arr[0]+k;
+        int highest=arr[n-1]-k;
+        for(int i=0;i<n-1;i++){
+            if (arr[i + 1] - k < 0) {
+                continue;
+            }
+
+            int minHeight = Math.min(lowest, arr[i + 1] - k);
+            int maxHeight = Math.max(highest, arr[i] + k);
+
+            ans = Math.min(ans, maxHeight - minHeight);
+        }
+
+        System.out.println("Minimum heights: " + ans);
     }
     public static void main(String args[]){
-
+        int arr[]={3, 9, 12, 16, 20};
+        int k=3;
+        helper(arr, k);
     }
 }
