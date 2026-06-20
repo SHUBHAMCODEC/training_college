23e287feb5ed7b7e3c88ec77c0749f4266389de6
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-20 16:40:49 +0000

    commenc

--- thoughtWorks/combinationSum.java
+++ thoughtWorks/combinationSum.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class combinationSum {
     
-    private static void helper(int ind,int[] arr,int target,List<List<Integer>> ls,List<Integer> curr){
+    private static void helper(int ind,int[] arr,int target,List<List<Integer>> ls,List<Integer> curr){// helper function
         if(ind==arr.length){
             if(target==0){
                 ls.add(new ArrayList<>(curr));
@@ -20,7 +20,7 @@ public class combinationSum {
         helper(ind+1, arr, target, ls, curr);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// main function for running code
         int arr[]={1,2,3};
         int target=5;
         List<List<Integer>> ls=new ArrayList<>();
