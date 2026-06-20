9b1ef24aeb8ef822efc3a4cf4dbbfa1c98ccf4f3
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-20 16:39:56 +0000

    newer day

---
+++ thoughtWorks/combinationSum.java
@@ -0,0 +1,31 @@
+package thoughtWorks;
+
+import java.util.*;
+
+public class combinationSum {
+    
+    private static void helper(int ind,int[] arr,int target,List<List<Integer>> ls,List<Integer> curr){
+        if(ind==arr.length){
+            if(target==0){
+                ls.add(new ArrayList<>(curr));
+            }
+            return;
+        }
+
+        if(arr[ind]<=target){
+            curr.add(arr[ind]);
+            helper(ind+1, arr, target-arr[ind], ls, curr);
+            curr.remove(curr.size()-1);
+        }
+        helper(ind+1, arr, target, ls, curr);
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,2,3};
+        int target=5;
+        List<List<Integer>> ls=new ArrayList<>();
+        helper(0, arr, target, ls,new ArrayList<>());
+
+        System.out.println(ls);
+    }
+}
