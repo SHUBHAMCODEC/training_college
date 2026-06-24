9cf1ec4b8ce95da910b3c2de29327e4e58d7619b
SHUBHAMCODEC <139606029+SHUBHAMCODEC@users.noreply.github.com>
2026-06-24 20:28:19 +0530

    Implement main method for testing duplicates removal
    Added a main method to test removeDuplicates function.

--- ARRAY/removeDuplcatesFromSortedArray.java
+++ ARRAY/removeDuplcatesFromSortedArray.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {// we have to remove the duplicates from sorted array
     public int removeDuplicates(int[] nums) {
         int k = 2;
 
@@ -10,5 +10,11 @@ class Solution {
         }
 
         return k;        
+    }
+
+    public static void main(String args[]){
+        int[] nums={1,1,2,3,4,5,5,6,10,12,12,15,17,22,22,25,25,36,45,46,48,48,50,50,50};
+        int[]arr2=removeDuplicates(nums);
+        System.out.println(arr2);
     }
 }
