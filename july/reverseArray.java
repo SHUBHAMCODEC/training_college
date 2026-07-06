c6504bbe0ad87d18512e9e6b9b027560d5960e78
Shubham Sharma <spandit70755@gmail.com>
2026-07-06 21:05:14 +0530

    more function

--- july/reverseArray.java
+++ july/reverseArray.java
@@ -19,7 +19,36 @@ public class reverseArray {
         }
     }
 
+
+    private static void max_min_element(int arr[]){
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        int actualMin=Integer.MAX_VALUE;
+        for(int i:arr){
+            
+            if(i>max){
+                min=max;
+                max=i;
+            }
+            else if(i<max && i>min){
+                min=i;
+            }
+        }
+        
+        for(int i:arr){
+
+          if(i<actualMin){
+            actualMin=i;
+          }
+        }
+        
+        System.out.println("max element is : "+ max);
+        System.out.println(" 2nd max element is : "+ min);
+        System.out.println(" actualMIn element is : "+ actualMin);
+        
+    }
     public static void main(String[] args) {
         helper(new int[]{1,2,3,4,50,60,70,80,90,100});
+        max_min_element(new arr[]{1,2,3,4,50,60,70,80,90,100});
     }
 }
