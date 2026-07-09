3da208ea86402584d64835ebcbbe9a7fb12fef4b
Shubham Sharma <spandit70755@gmail.com>
2026-07-09 20:42:45 +0530

    more message

--- july/median.java
+++ july/median.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
-class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+class median {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         ArrayList<Integer> list = new ArrayList<>();
         
         // 1. Merge both arrays
@@ -13,12 +13,16 @@ class Solution {
 
         int n = list.size();
         if (n % 2 != 0) {
-            // Odd: Return the middle element
+            
             return list.get(n / 2);
         } else {
             // Even: Average of the two middle elements
             // Use 2.0 to ensure double division
             return (list.get(n / 2) + list.get(n / 2 - 1)) / 2.0;
         }
+    }
+
+    public static void main(String[] args){
+        System.out.println(findMedianSortedArrays(new int[]{1,5,7,8,9},{78,45,12,56,55,89,80}));
     }
 }
