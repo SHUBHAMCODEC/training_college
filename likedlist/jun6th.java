f71053d734a3bcd837e48a52f9e316dbc9575f0d
Shubham Sharma <spandit70755@gmail.com>
2026-07-10 21:05:17 +0530

    newer daty

--- likedlist/jun6th.java
+++ likedlist/jun6th.java
@@ -1,5 +1,18 @@
 package likedlist;
 
 public class jun6th {
-    
+    static class Node{
+
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    private static void creation(int [] arr){
+
+    }
 }
