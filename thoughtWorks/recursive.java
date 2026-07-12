37c1e10ba3d9001b1d639f7b30d41c4c37a22167
Shubham Sharma <spandit70755@gmail.com>
2026-07-12 15:59:30 +0530

    more practice

---
+++ thoughtWorks/recursive.java
@@ -0,0 +1,48 @@
+package thoughtWorks;
+import java.util.*;
+public class recursive {
+
+
+    private static List<List<Integer>> helperBit(int arr[]){
+        int num=arr.length;
+        int n=1<<num;
+        List<List<Integer>> mainList=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            List<Integer> subList=new ArrayList<>();
+            for(int j=0;j<num;j++){
+                if(((i&(1<<j))!=0)){
+                    subList.add(arr[j]);
+                }
+            }
+            mainList.add(subList);
+        }
+
+        return mainList;
+    }
+
+
+   
+    public static void main(String[] args) {
+
+        int[] input = {1, 2, 3};
+
+        // 1. Testing Bit Manipulation Approach
+        List<List<Integer>> subsets = helperBit(input);
+        
+        System.out.println("All generated subsets via Bit Manipulation:");
+        for (List<Integer> subset : subsets) {
+            System.out.println(subset);
+        }
+
+        System.out.println("\n--------------------------------------------------\n");
+        
+        // 2. Testing Recursive Approach
+        List<List<Integer>> subsetsRecursive = new ArrayList<>();
+        helperByrecursion(0, input, subsetsRecursive, new ArrayList<>());
+        
+        System.out.println("All generated subsets via Recursion (Pick / Not Pick):");
+        for (List<Integer> subset : subsetsRecursive) {
+            System.out.println(subset);
+        }
+    }
+}
