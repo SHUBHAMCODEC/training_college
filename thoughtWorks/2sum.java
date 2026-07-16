f4aa46052e6d5165e57e14d38d9ca44be75f699f
Shubham Sharma <spandit70755@gmail.com>
2026-07-16 22:36:27 +0530

    more

--- thoughtWorks/2sum.java
+++ thoughtWorks/2sum.java
@@ -1,7 +1,23 @@
 package thoughtWorks;
-
+import java.util.*;
 public class 2sum {
-    public static void main(String[] args) {
+
+    private static void helper(int arr[],int target){
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            int remain=target-arr[i];
+            if(mpp.containsKey(remain)){
+                System.out.println("indeces are"+ mpp.get(remain)+" and "+ i);
+                return;
+            }
+
+            mpp.put(arr[i],i);
+        }
+
         
+    }
+    public static void main(String[] args) {
+       helper(new int[]{1,2,3,47,15,13,149,20}, 23);
     }
 }
