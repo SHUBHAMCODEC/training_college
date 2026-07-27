0b3c98d2803ea2f4c12f9da7be33898e4fdadb14
Shubham Sharma <spandit70755@gmail.com>
2026-07-27 18:55:47 +0530

    level 1

--- thoughtWorks/Monday.java
+++ thoughtWorks/Monday.java
@@ -1,6 +1,6 @@
 package thoughtWorks;
 // Arrays - Fundamentals & Problem Patterns
-
+import java.util.*;
 public class Monday {
 
     private static int Largest_Element_in_an_Array(int arr[]){
@@ -71,9 +71,98 @@ public class Monday {
         
             arr[n-1]=temp;
         }
-        
+
         return arr;
             
+    }
+    private static int[] Move_All_Zeroes_to_End(int [] arr){
+        int index=0;
+
+        for(int i:arr){
+            if(i>0){
+                arr[index++]=i;
+            }
+        }
+        for(int j=index;j<arr.length;j++){
+            arr[j]=0;
+        }
+
+        return arr;
+    }
+    private static void Remove_Duplicates_from_Sorted_Array(int [] arr){
+        int low=1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i-1]==arr[i]){
+                continue;
+            }else{
+                arr[low++]=arr[i];
+            }
+        }
+
+        System.out.print("updated Array Elements are: ");
+        for(int i=0;i<low;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        System.out.println("\n");
+    }
+
+    private static ArrayList<Integer> Merge_Two_Sorted_Arrays(int ar1[],int ar2[]){
+        int n=ar1.length;
+        int m=ar2.length;
+
+        ArrayList<Integer> result=new ArrayList<>();
+
+        int i=0,j=0;
+        while(i<n && j<m){
+            if(ar1[i]<ar2[j]){
+                result.add(ar1[i++]);
+            }else{
+                result.add(ar2[j++]);
+            }
+        }
+
+        while(i!=n){
+        result.add(ar1[i++]);
+        }
+        while(j!=m){
+            result.add(ar2[j++]);
+        }
+
+        return result;
+    }
+
+    private static int[] Two_Sum(int arr[],int target){
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            int remains=target-arr[i];
+            if(mpp.containsKey(remains)){
+                int index=mpp.get(remains);
+                return new int[]{i,index};
+            }
+
+            mpp.put(arr[i],i);
+        }
+
+        for(Map.Entry<Integer,Integer> i: mpp.entrySet()){//extra for just sake of visualising the Map
+            System.out.println("key: "+i.getKey()+" and value: "+ i.getValue());
+        }
+
+        return new int[]{-1,-1};
+    }
+
+    private static boolean Pair_with_Given_Sum_Sorted_Array(int arr[],int sum){
+        int low=0, high=arr.length-1;
+        while(low<high){
+            if((arr[low]+arr[high])==sum){
+                return true;
+            }else if((arr[low]+arr[high])<sum){
+                low++;
+            }else{
+                high--;
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
         
