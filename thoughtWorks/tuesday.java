8caa6f30b011fb776283cc20661718f536c01896
Shubham Sharma <spandit70755@gmail.com>
2026-07-28 19:29:33 +0530

    level 2

--- thoughtWorks/tuesday.java
+++ thoughtWorks/tuesday.java
@@ -93,6 +93,102 @@ public class tuesday {
             System.out.println("Character: " + e.getKey() + ", Count: " + e.getValue());
         }
     }
+
+    private static void anagram(String s,String t){
+        char[] c1=new char[26];
+
+        for(int i=0;i<s.length();i++){
+            c1[(s.charAt(i))-'a']+=1;
+        }
+        for(int i=0;i<t.length();i++){
+            c1[(t.charAt(i))-'a']-=1;
+        }
+
+        for(int i=0;i<c1.length;i++){
+            if(c1[i]!=0){
+                System.out.println("strings are not anagram in nature...");
+                return;
+            }
+        }
+
+        System.out.println("Strings are not anagram in nature... ");
+
+    }
+
+    private static void First_Non_Repeating_Character(String s){
+        HashMap<Character,Integer>mpp=new HashMap<>();
+
+        for(char i:s.toCharArray()){
+            mpp.put(i,mpp.getOrDefault(i, 0)+1);
+        }
+
+        for(Map.Entry<Character,Integer>e:mpp.entrySet()){
+            if(e.getValue()==1){
+                System.out.println("First_Non_Repeating_Character :"+ e.getKey());
+            }
+        }
+    }
+
+    private static void Longest_Common_Prefix(String[] s) {
+        StringBuilder sb = new StringBuilder();
+    
+        if (s == null || s.length == 0) {
+            System.out.println("Longest Common Prefix: ");
+            return;
+        }
+    
+        for (int i = 0; i < s[0].length(); i++) {
+            char c = s[0].charAt(i);
+    
+            for (int j = 1; j < s.length; j++) {
+                
+                if (i >= s[j].length() || c != s[j].charAt(i)) {
+                    System.out.println("Longest Common Prefix: " + sb.toString());
+                    return;
+                }
+            }
+            sb.append(c);
+        }
+        
+        
+        System.out.println("Longest Common Prefix: " + sb.toString());
+    }
+    private static void String_Compression(String s){
+        StringBuilder sb=new StringBuilder();
+        int cnt=0;
+        char c[]=s.toCharArray();
+        for(int i=0;i<c.length;i++){
+            cnt+=1;
+            if(c[i]==c[i+1]){
+                continue;
+            }else{
+                sb.append(c[i]);
+                sb.append(cnt);
+                cnt=0;
+            }
+        }
+
+        System.out.println("String Compression: "+ sb.toString());
+    }
+
+    private static boolean Check_Prime_Number(int n){
+        for(int i=2;i<Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void allFactors(int n){
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+               System.out.print(i+" "); 
+            }
+        }
+    }
+    
+    
     public static void main(String[] args) {
         
     }
