d28de5dc95375aed866a828527b63e884d67e110
Shubham Sharma <spandit70755@gmail.com>
2026-07-29 16:32:29 +0530

    quick error fixing

--- thoughtWorks/wednesday.java
+++ thoughtWorks/wednesday.java
@@ -11,9 +11,9 @@ public class wednesday {
         for(int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j] > arr[j + 1]){
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+                    int temp=arr[j];
+                    arr[j]=arr[j + 1];
+                    arr[j + 1]=temp;
                 }
             }
         }
@@ -25,21 +25,18 @@ public class wednesday {
     }
     
     private static void SelectionSort(int [] arr){
-        
         System.out.print("ArrayBefore Selection Sort: ");
         for(int i:arr){
             System.out.print(i+" ");
         }
         System.out.println();
         for(int i=0;i<arr.length-1;i++){
-            
             int minIndex=i;
-            for(int j=i+1;j<arr.length-1;j++){
+            for(int j=i+1;j<arr.length;j++){
                 if(arr[j] < arr[minIndex]){
                     minIndex=j;
                 }
             }
-            
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
@@ -57,10 +54,9 @@ public class wednesday {
             System.out.print(i+" ");
         }
         System.out.println();
-        for(int i=0;i<arr.length;i++){
+        for(int i=1;i<arr.length;i++){
             int key = arr[i];
-            
-            int j=i;
+            int j=i-1;
             while(j>=0 && arr[j]>key){
                 arr[j + 1] = arr[j];
                 j--;
@@ -75,24 +71,12 @@ public class wednesday {
     }
 
     private static void mergeSort(int[] arr,int low,int high){
-        
-        System.out.print("ArrayBefore Merge Sort: ");
-        for(int i:arr){
-            System.out.print(i+" ");
-        }
-        System.out.println();
         if (low < high) {
             int mid = low + (high - low) / 2;
             mergeSort(arr, low, mid);
             mergeSort(arr, mid + 1, high);
             merger(arr, low, mid, high);
         }
-
-        System.out.print("ArrayAfter Merge Sort: ");
-        for(int i:arr){
-            System.out.print(i+" ");
-        }
-        System.out.println();
     }
 
     private static void merger(int [] arr,int low,int mid,int high){
@@ -103,7 +87,7 @@ public class wednesday {
         int[] arr2=new int[m];
 
         for(int i=0;i<n;i++){
-            arr1[i]=arr[low+1];
+            arr1[i]=arr[low+i];
         }
         for(int i=0;i<m;i++){
             arr2[i]=arr[mid+i+1];
@@ -112,7 +96,7 @@ public class wednesday {
         int i=0,j=0;
         int k=low;
         while(i<n && j<m){
-            if(arr1[i]<arr2[j]){
+            if(arr1[i]<=arr2[j]){
                 arr[k++]=arr1[i++];
             }else{
                 arr[k++]=arr2[j++];
@@ -129,10 +113,11 @@ public class wednesday {
     private static void QuickSort(int arr[],int low,int high){
         if (low < high) {
             int pIndex = partition(arr, low, high);
-            quickSort(arr, low, pIndex - 1);
-            quickSort(arr, pIndex + 1, high);
+            QuickSort(arr, low, pIndex - 1);
+            QuickSort(arr, pIndex + 1, high);
         }
     }
+    
     private static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = low - 1;
@@ -150,12 +135,9 @@ public class wednesday {
         return i + 1;
     }
 
-
     private static void DutchFlag(int arr[]){
+        System.out.println("Before Dutch Flag: " + Arrays.toString(arr));
         int low=0, mid=0,high=arr.length-1;
-
-       System.out.println("Before Dutch Flag: " + Arrays.toString(arr));
-      
         while (mid <= high) {
             if (arr[mid] == 0) {
                 int temp = arr[low];
@@ -174,7 +156,23 @@ public class wednesday {
         }
         System.out.println("After Dutch Flag:  " + Arrays.toString(arr));
     }
+
     public static void main(String[] args) {
+        int[] data = {4, 2, 1, 3, 0, 2};
+        bubbleSort(data.clone());
+        SelectionSort(data.clone());
+        insertionSort(data.clone());
         
+        int[] mData = data.clone();
+        System.out.println("Before Merge Sort: " + Arrays.toString(mData));
+        mergeSort(mData, 0, mData.length - 1);
+        System.out.println("After Merge Sort:  " + Arrays.toString(mData));
+        
+        int[] qData = data.clone();
+        System.out.println("Before Quick Sort: " + Arrays.toString(qData));
+        QuickSort(qData, 0, qData.length - 1);
+        System.out.println("After Quick Sort:  " + Arrays.toString(qData));
+        
+        DutchFlag(new int[]{2, 0, 1, 2, 0, 1});
     }
 }
