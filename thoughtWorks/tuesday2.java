96d3507e932a75401127b2e618f1f5482dc8cdb8
Shubham Sharma <spandit70755@gmail.com>
2026-08-05 19:41:22 +0530

    more in LinkedlIst

--- thoughtWorks/tuesday2.java
+++ thoughtWorks/tuesday2.java
@@ -116,8 +116,126 @@ public class tuesday2 {
         System.out.println("Middle List is "+ slow.data);
     }
 
-    private static void detectCycle(Node head){
+    private static boolean detectCycle(Node head){
+        Node slow=head,fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static void firstOfCycle(Node head){
+        Node slow=head,fast=head;
+
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+
+                System.out.println("First Element of Cycle is: "+ slow.data);
+            }
+        }
+    }
+
+    private static void removeNthNode(Node head,int target){
         
+
+        Node revTemp=reverseByrecursion(head);
+        int count=1;
+        Node prev=null,temp=revTemp;
+        while(count!=target){
+            count++;
+            prev=temp;
+            temp=temp.next;
+        }
+        prev.next=temp.next;
+
+        Node OriTemp=reverseByrecursion(revTemp);
+
+        printll(OriTemp);
+    }
+
+
+    private static Node MergeTwoList(Node list1,Node list2){
+        Node DummyHead=new Node(-1);
+        Node dummyTemp=DummyHead;
+        Node temp1=list1,temp2=list2;
+
+        if(temp1==null){
+            return temp2;
+        }else if(temp2==null){
+            return temp1;
+        }
+        while(temp1!=null && temp2!=null){
+            if(temp1.data<temp2.data){
+                dummyTemp.next=temp1;
+                dummyTemp=dummyTemp.next;
+                temp1=temp1.next;
+            }else{
+                dummyTemp.next=temp2;
+                dummyTemp=dummyTemp.next;
+                temp2=temp2.next;
+                
+                
+            }
+        }
+        while(temp1!=null){
+            dummyTemp.next=temp1;
+            dummyTemp=dummyTemp.next;
+            temp1=temp1.next;
+
+        }while(temp2!=null){
+            dummyTemp.next=temp2;
+            dummyTemp=dummyTemp.next;
+            temp2=temp2.next;
+        }
+
+        return DummyHead.next;
+    }
+
+    private static Node removeDuplicates(Node head){
+        Node curr=head.next,prev=head;
+        while(curr!=null){
+            if(curr.data!=prev.data){
+                prev.next=curr;
+                prev=prev.next;
+                curr=curr.next;
+            }
+        }
+
+        return prev;
+
+    }
+
+    private static void OddEvenList(Node head){
+        Node OddHead=head.next; Node EvenHead=head;
+        Node tempOdd=OddHead; Node tempEven=EvenHead;
+        while(tempOdd!=null && tempOdd.next!=null){
+            tempOdd.next=tempOdd.next.next;
+            tempOdd=tempOdd.next;
+        }
+        while(tempEven!=null && tempEven.next!=null){
+            tempEven.next=tempEven.next.next;
+            tempEven=tempEven.next;
+        }
+
+        if(tempOdd==null || tempOdd.next==null){
+            tempOdd.next=null;
+        }else{
+            tempEven.next=null;
+        }
     }
     public static void main(String[] args) {
         creation(new int[]{1,2,3,4,5,6});
